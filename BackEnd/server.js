58bc6a4bb91e50a63a45bb056ba50a89d51063a8
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2024-11-09 01:57:52 +0000

    Comments server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,34 +1,39 @@
-const express = require('express');
+const express = require('express'); //importing Express library to set up web server
 const app = express();
-const port = 4000;
+const port = 4000; //define port on which server will listen
 
 /*
     CORS - allows frontend and backend to run on different domains/ports
+    CORS is a security feature in web dev that allows or restricts resources on a web page
+    from being requested from another domain
 */
 const cors = require('cors');
-app.use(cors());
+app.use(cors()); //applying CORS to allow cross-origin requests
 
+//Manually set headers
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header("Access-Control-Allow-Origin", "*"); //allows any domain to access API
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); //specifies allowed HTTP methods
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); //specifies allowed headers
+  next(); //passes control to the next middleware function
 });
 
-//allows json to parse info out of http request
+//bodyParser to parse incoming requests - extracts JSON data
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); //parses URL-encoded data
+app.use(bodyParser.json()); //parses JSON data from the request body for easier access
 
-
+//basic GET route at URL '/'
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+//POST route to add new movie
 app.post('/api/movies', (req, res) => {
     res.send('movie added');
 });
 
+//GET route to retrieve list of movies
 app.get('/api/movies', (req, res) => {
     const movies = [
         {
@@ -53,9 +58,10 @@ app.get('/api/movies', (req, res) => {
             "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
         }
     ];
-    res.status(200).json({ movies });
+    res.status(200).json({ movies }); //sends back movies array as JSON
 });
 
+//starts server on specified port and logs message to console
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
